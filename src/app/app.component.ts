0974efb4f130ec25d457774ca779dc7571eb8ca6
eoko <eoko>
2018-02-28 17:46:32 +0100

    Ajout des couleurs et icons du menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,16 +16,16 @@ export class MyApp {
 
   rootPage: any = ViewHomePage;
 
-  pages: Array<{title: string, component: any}>;
+	pages: Array<{title: string, component: any, icon: string}>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Boîte aux cartes', component: ViewHomePage },
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Boîte aux cartes', component: ViewHomePage, icon: 'photos' },
+      { title: 'Home', component: HomePage, icon: 'home' },
+      { title: 'List', component: ListPage, icon: 'trophy' }
     ];
 
   }
