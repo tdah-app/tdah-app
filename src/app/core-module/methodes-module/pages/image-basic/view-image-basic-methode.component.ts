d9b24e8d06e48c372c4d65ab9a0ae4af2df6b8b8
eoko <eoko>
2018-03-16 16:10:21 +0100

    Modifiation image methode

--- src/app/core-module/methodes-module/pages/image-basic/view-image-basic-methode.component.ts
+++ src/app/core-module/methodes-module/pages/image-basic/view-image-basic-methode.component.ts
@@ -24,7 +24,6 @@ export class ViewImageBasicMethode implements OnInit {
     this.i = navParams.get("index");
   }
 
-
   ngOnInit() {
     if (this.myMethode.listeElements.length != 0)
       this.nomBouton = this.myMethode.listeElements[this.i].bouton;
@@ -35,38 +34,14 @@ export class ViewImageBasicMethode implements OnInit {
     this.image = (<ElementImage>this.myMethode.listeElements[this.i]).image;
   }
 
-
   boutonTapped() {
     // on regarde si la liste d'élements de la carte n'a pas été entièrement parcourue
     if (this.i < this.myMethode.listeElements.length - 1) {
       this.i = this.i + 1;
-
       this.navCtrl.push(Utils.getNextPage(this.myMethode, this.i), {
         resultParam: this.myMethode,
         index: this.i
       });
-
-      // on teste le type de l'élement suivant afin de savoir quelle view ouvrir
-      //  if(this.myMethode.listeElements[this.i].type == 'ElementSavaisTuQue') {
-      //     this.navCtrl.push(ViewBasicMethode, {
-      //     resultParam: this.myMethode,
-      //     index: this.i
-      //     });
-      // }
-
-      // else if(this.myMethode.listeElements[this.i].type == 'ElementImage') {
-      //     this.navCtrl.push(ViewImageBasicMethode, {
-      //     resultParam: this.myMethode,
-      //     index: this.i
-      //     });
-      // }
-
-      //   else if(this.myMethode.listeElements[this.i].type == 'Element') {
-      //     this.navCtrl.push(ViewBasicMethode, {
-      //     resultParam: this.myMethode,
-      //     index: this.i
-      //     });
-      // }
     }
     // si la liste d'élements de la carte a été entièrement parcourue
     else {
